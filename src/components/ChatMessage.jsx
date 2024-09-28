3bc60fbdc92f3f2365b98c00394710b05c041761
PlaceHolderHere <holderhereinplace@gmail.com>
2024-09-28 20:03:20 +0800

    Fixed problem with really long message getting rendering only as 1 line and being cut off

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -7,7 +7,7 @@ export const ChatMessage = (props) =>{
     if (currentUser == user){
         return(
         <div className="flex justify-end ml-auto items-center m-4 max-w-lg">
-            <p className="py-2 px-4 m-1 text-base bg-blue-100 rounded-lg break-words">
+            <p className="py-2 px-4 m-1 text-base bg-blue-100 rounded-lg whitespace-normal overflow-auto break-words">
                 {messageValue}
             </p>
             <img alt="current-user-photo" src={photo} className="w-10 h-10 object-cover rounded-full"/>
@@ -19,7 +19,7 @@ export const ChatMessage = (props) =>{
             <h1 className="text-md break-words">{user}</h1>
             <div className="flex items-center max-w-lg">
                 <img alt="user-photo" src={photo} className="w-10 h-10 object-cover rounded-full"/>
-                <p className="py-2 px-4 m-1 text-base bg-neutral-100 rounded-lg break-words">
+                <p className="py-2 px-4 m-1 text-base bg-neutral-100 rounded-lg whitespace-normal overflow-auto break-words">
                     {messageValue}
                 </p>
             </div>
--- src/components/GroupChatButton.jsx
+++ src/components/GroupChatButton.jsx
@@ -9,7 +9,7 @@ export const GroupChatButton = (props) =>{
 
     return(
         <button onClick={updateGroupChatId} className="p-4 flex items-center bg-slate-100 m-1 rounded-md w-11/12 hover:bg-slate-200">
-            <h1 className="text-md break-words">{groupChatName}</h1>
+            <h1 className="text-md break-words whitespace-normal overflow-auto">{groupChatName}</h1>
         </button>
     )
 }
